e32dec769f571ef215c9f0ee662b5417a9fe158b
Neev Agarwal <agarwalneev2005@gmail.com>
2025-02-17 18:31:42 +0530

    Ai Code Reviewer Completed

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const aiRoutes = require('./Routes/aiRoutes');
 const app = express();
+const cors = require('cors')
+
+app.use(cors())
 
 app.use(express.json());        //IMp for running request.body command else it will return undefined
 
--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,26 +1,67 @@
-import { useState } from 'react'
+import {useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import "prismjs/themes/prism-tomorrow.css"
+import prism, { highlightAll } from "prismjs"
+import Editor from 'react-simple-code-editor'
+import axios from 'axios'
+import Markdown from 'react-markdown'
 
 function App() {
   const [count, setCount] = useState(0)
 
+
+  useEffect(() => {
+    prism.highlightAll()
+    
+  })
+  const [code, setCode] = useState(`function sum() {return 1+1}`)
+  const [review, setReview] = useState()
+
+  async function reviewCode(){
+    const response = await axios.post('http://localhost:3000/ai/get-review' ,{code} )
+    setReview(response.data)
+    // console.log(response.data)
+  }
+
   return (
     <>
-    <div className="main">
+    <main>
 
       <div className="left">
-        <div className="code"></div>
-        <div className="btn"></div>
+
+        <div className="code">
+          <Editor
+            value={code}
+            onValueChange={code => setCode(code)}
+            highlight={code => prism.highlight(code , prism.languages.javascript , 'javascript')}
+            padding={10}
+            style={{
+              fontFamily:`"Fira code" , "Fira Mono" , monospace`,
+              fontsize:16,
+              border: '1px solid #ddd',
+              borderRadius:"5px",
+              height:'100%',
+              width:"100%"
+            }}
+          />
+        </div>
+
+
+        <div onClick={reviewCode} className="btn">Review</div>
       </div>
 
 
 
       <div className="right">
+        <Markdown  >
+        {review}
+        </Markdown> 
+        
       </div>
 
-    </div>
+    </main>
     </>
   )
 }
